6f52670ecf77c12af9eeead735e4d133b534e8a2
Montassar7 <montassar7bouazizi@gmail.com>
2024-12-09 00:31:38 +0100

    refactor soap & services

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import register_view, login_view
-from .views import django_soap_app  # Import the soap app
+from user.views.soap.user_views import django_soap_app
+
 
 urlpatterns = [
-    path('register/', register_view, name='register'),
-    path('login/', login_view, name='login'),
+    # path('register/', register_view, name='register'),
+    # path('login/', login_view, name='login'),
     path('soap/', django_soap_app, name='soap'),  # This line maps /soap/ to the SOAP service
 ]
---
+++ user/views/soap/user_views.py
@@ -0,0 +1,129 @@
+from spyne import Application, rpc, ServiceBase, Unicode, Fault, Integer
+from spyne.protocol.soap import Soap11
+from spyne.server.django import DjangoApplication
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth.hashers import make_password
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError, ObjectDoesNotExist
+from django.db import IntegrityError
+from user.complexTypes import User as ComplexUser
+from user.services.user_services import UserService
+from spyne.model.primitive import AnyDict  
+
+VALID_ROLES = ["manager", "employee"]  
+
+User = get_user_model()
+
+user_service = UserService()
+
+
+def validate_credentials(user: ComplexUser):
+    try:
+        validate_email(user.email)
+    except ValidationError:
+        raise Fault(faultcode="Client", faultstring="Invalid email format.")
+    if not user.first_name or not user.last_name:
+        raise Fault(faultcode="Client", faultstring="First name and last name are required.")
+    if not user.password:
+        raise Fault(faultcode="Client", faultstring="Password is required.")
+
+def validate_user(user: ComplexUser):
+    validate_credentials(user)
+    if user.role not in VALID_ROLES:
+        raise Fault(faultcode="Client", faultstring=f"Invalid role. Valid roles: {', '.join(VALID_ROLES)}")
+
+def validate_admin(user: ComplexUser):
+    validate_credentials(user)
+    if user.role != "admin":
+        raise Fault(faultcode="Client", faultstring=f"Invalid role !")
+
+
+
+
+class UserSOAPService(ServiceBase):
+    @rpc(ComplexUser, _returns=Unicode)
+    def register_user(self, user: ComplexUser):
+        try:
+            validate_user(user)  
+            new_user = user_service.create_user(user)
+            return f"User {user.username} registered successfully with role {user.role}!"
+        except Exception as e:
+            raise Fault(faultcode="Server", faultstring=str(e))
+        
+
+    @rpc(ComplexUser, _returns=Unicode)
+    def register_admin(self, user: ComplexUser):
+        try:
+            validate_admin(user)
+            new_user = user_service.create_user(user)
+            return f"Admin {user.username} registered successfully!"
+        except IntegrityError:
+            raise Fault(faultcode="Client", faultstring="Username or email already exists.")
+        except Exception as e:
+            raise Fault(faultcode="Server", faultstring="An unexpected error occurred.")
+
+
+    @rpc(Unicode,Unicode, _returns=ComplexUser)
+    def login_user(self, username, password):
+        """Login user and return role-specific redirect path."""
+        user_obj = authenticate(username=username, password=password)
+        if user_obj:
+            # Define redirect paths for different roles
+            redirect_paths = {
+                "employee": "/dashboard/employee",
+                "manager": "/dashboard/manager",
+                "admin": "/dashboard/admin",
+            }
+            redirect_path = redirect_paths.get(user_obj.role, "/dashboard")
+
+            return ComplexUser(username=user_obj.username, email=user_obj.email, role=user_obj.role, redirect_path=redirect_path)
+        raise Fault(faultcode="Client", faultstring="Invalid credentials")
+
+    @rpc(ComplexUser, _returns=ComplexUser)
+    def update_user(self, user: ComplexUser):
+        """Update user details."""
+        try:
+            validate_user(user)  # Validate user input
+            updated_user = user_service.update_user(user)
+            return updated_user
+        except User.DoesNotExist:
+            raise ObjectDoesNotExist(f"User {user.username} does not exist.")
+
+    @rpc(ComplexUser, _returns=ComplexUser)
+    def update_admin(self, user: ComplexUser):
+        try:
+            validate_admin(user)
+            updated_user = user_service.update_user(user)
+            return updated_user
+        except User.DoesNotExist:
+            raise ObjectDoesNotExist(f"User {user.username} does not exist.")
+
+    @rpc(Integer, AnyDict, _returns=ComplexUser)  # Specify the types
+    def patch_user(self, userId: int, updated_data: dict):
+        try:
+            updated_user = user_service.patch_user(userId, updated_data)
+            return updated_user
+        except User.DoesNotExist:
+            raise ObjectDoesNotExist(f"User with ID {userId} does not exist.")
+    # Todo validate userId is admin
+    @rpc(int, _returns=None)
+    def delete_user(self, userId: int):
+        try:
+            user_service.delete_user(userId)
+        except User.DoesNotExist:
+            raise ObjectDoesNotExist(f"User with ID {userId} does not exist.")
+          
+    
+    
+
+# SOAP Application Setup
+soap_app = Application(
+    [UserSOAPService],
+    tns="project.user",
+    in_protocol=Soap11(validator="lxml"),
+    out_protocol=Soap11(),
+)
+
+# Django Soap App with CSRF exemption
+django_soap_app = csrf_exempt(DjangoApplication(soap_app))
